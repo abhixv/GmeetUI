9dff48dec3fa8b865f20330159897a21b6cfbba0
abhixv <abhi.sharma1@hotmail.com>
2021-09-01 09:45:31 +0530

    Add Meeting code search Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gmeet/ui/home_page.dart';
+import 'package:gmeet/ui/meeting_search.dart';
+import 'package:gmeet/utils/routes.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,6 +14,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
+      initialRoute: MyRoutes.homeRoute,
+      routes: {
+        MyRoutes.meetingJoin: (context) => MeetingJoin(),
+      },
     );
   }
 }
