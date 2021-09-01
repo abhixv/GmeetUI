e11f16446bff43ac433990237013f6c88eda2985
abhixv <abhi.sharma1@hotmail.com>
2021-09-01 09:00:22 +0530

    Add Bottom popup Card and fixup

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -8,13 +8,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  double _height = 0.15;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff202025),
+      backgroundColor: Colors.black,
       drawer: Drawer(),
       appBar: AppBar(
-        backgroundColor: Color(0xff202025),
+        backgroundColor: Colors.black,
         iconTheme: IconThemeData(
           color: Color(0xfff9f9fa),
         ),
@@ -37,41 +38,49 @@ class _HomePageState extends State<HomePage> {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.4,
-                    height: MediaQuery.of(context).size.height * 0.05,
-                    decoration: BoxDecoration(
-                        color: Color(0xff89b5f9),
-                        border:
-                            Border.all(color: Color(0xfff9f9fa), width: 0.7),
-                        borderRadius: BorderRadius.circular(5)),
-                    child: Center(
-                      child: Text(
-                        "New meeting",
-                        style: TextStyle(
-                            color: Color(0xff1f2a52),
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Avenir'),
+                  InkWell(
+                    onTap: () {
+                      _popcard(context);
+                    },
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      height: MediaQuery.of(context).size.height * 0.05,
+                      decoration: BoxDecoration(
+                          color: Color(0xff89b5f9),
+                          border:
+                              Border.all(color: Color(0xfff9f9fa), width: 0.7),
+                          borderRadius: BorderRadius.circular(5)),
+                      child: Center(
+                        child: Text(
+                          "New meeting",
+                          style: TextStyle(
+                              color: Color(0xff1f2a52),
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Avenir'),
+                        ),
                       ),
                     ),
                   ),
                   SizedBox(
                     width: 12,
                   ),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.4,
-                    height: MediaQuery.of(context).size.height * 0.05,
-                    decoration: BoxDecoration(
-                        border:
-                            Border.all(color: Color(0xfff9f9fa), width: 0.7),
-                        borderRadius: BorderRadius.circular(5)),
-                    child: Center(
-                      child: Text(
-                        "Join with a code",
-                        style: TextStyle(
-                            color: Color(0xff89b5f9),
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Avenir'),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      height: MediaQuery.of(context).size.height * 0.05,
+                      decoration: BoxDecoration(
+                          border:
+                              Border.all(color: Color(0xfff9f9fa), width: 0.7),
+                          borderRadius: BorderRadius.circular(5)),
+                      child: Center(
+                        child: Text(
+                          "Join with a code",
+                          style: TextStyle(
+                              color: Color(0xff89b5f9),
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Avenir'),
+                        ),
                       ),
                     ),
                   ),
@@ -82,7 +91,7 @@ class _HomePageState extends State<HomePage> {
               ),
               Container(
                   width: MediaQuery.of(context).size.width * 0.9,
-                  height: MediaQuery.of(context).size.height * 0.15,
+                  height: MediaQuery.of(context).size.height * _height,
                   decoration: BoxDecoration(
                       border: Border.all(color: Color(0xfff9f9fa), width: 0.7),
                       borderRadius: BorderRadius.circular(8)),
@@ -135,14 +144,21 @@ class _HomePageState extends State<HomePage> {
                                       SizedBox(
                                         width: 20,
                                       ),
-                                      Container(
-                                        alignment: Alignment.center,
-                                        child: Text(
-                                          "Dismiss",
-                                          style: TextStyle(
-                                              color: Color(0xfff9f9fa),
-                                              fontWeight: FontWeight.w500,
-                                              fontFamily: 'Avenir'),
+                                      InkWell(
+                                        onTap: () {
+                                          setState(() {
+                                            _height = 0;
+                                          });
+                                        },
+                                        child: Container(
+                                          alignment: Alignment.center,
+                                          child: Text(
+                                            "Dismiss",
+                                            style: TextStyle(
+                                                color: Color(0xfff9f9fa),
+                                                fontWeight: FontWeight.w500,
+                                                fontFamily: 'Avenir'),
+                                          ),
                                         ),
                                       ),
                                     ],
@@ -176,5 +192,114 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
     );
+  }
+
+  void _popcard(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext c) {
+          return Container(
+            padding: EdgeInsets.all(12),
+            height: MediaQuery.of(context).size.height * 0.24,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.black,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 15),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.link,
+                        size: 28,
+                        color: Color(0xfff9f9fa),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        "Get a meeting link to share",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Color(0xfff9f9fa),
+                          fontFamily: 'Avenir',
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 15),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.video_call,
+                        size: 28,
+                        color: Color(0xfff9f9fa),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        "Start an instant meeting",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Color(0xfff9f9fa),
+                          fontFamily: 'Avenir',
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 15),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 5),
+                        child: Icon(
+                          Icons.calendar_today,
+                          size: 18,
+                          color: Color(0xfff9f9fa),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Text(
+                        "Schedule in Google Calendar",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Color(0xfff9f9fa),
+                          fontFamily: 'Avenir',
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.close_sharp,
+                      size: 28,
+                      color: Color(0xfff9f9fa),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Close",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Color(0xfff9f9fa),
+                        fontFamily: 'Avenir',
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
